96a559cf79b639eb4a72b57190f4a1b3ee0ee707
HijosdelaFe <hijosdelafe.web@gmail.com>
2024-09-25 19:13:52 -0300

    agregado indicador de boton seleccionado en componente: quienes-somos

--- src/app/components/quienes-somos/quienes-somos.component.ts
+++ src/app/components/quienes-somos/quienes-somos.component.ts
@@ -1,13 +1,4 @@
 import { Component } from '@angular/core';
-// import { quienesSomos } from 'src/assets/quienes_somos/contenido/contenido-quienes_somos';
-// import { queHacemos } from 'src/assets/quienes_somos/contenido/contenido-quienes_somos';
-// import { porqueLoHacemos } from 'src/assets/quienes_somos/contenido/contenido-quienes_somos';
-// import { nuestrosObjetivos } from 'src/assets/quienes_somos/contenido/contenido-quienes_somos';
-// import { nuestrosValores } from 'src/assets/quienes_somos/contenido/contenido-quienes_somos';
-// import { artistas } from 'src/assets/quienes_somos/contenido/contenido-quienes_somos';
-// import { artistasList } from 'src/assets/quienes_somos/contenido/contenido-quienes_somos';
-
-
 @Component({
   selector: 'app-quienes-somos',
   templateUrl: './quienes-somos.component.html',
@@ -21,32 +12,12 @@ export class QuienesSomosComponent {
   imagenActual: string = '';
 
   constructor() {
-    //this.selectedInfo = 'info1';
-    this.mostrarInfo("assets/quienes_somos/contenido/quienes_somos.png");
+    this.selectedInfo = 'info1';
+    this.mostrarInfo("assets/quienes_somos/contenido/quienes_somos.png", this.selectedInfo);
   }
 
-  mostrarInfo(imagen: string) {
-    // this.selectedInfo = info;
-    // if (info == "info1") {
-    //   // this.contenidoInfo = quienesSomos;
-    //   // this.imagenActual = 
-    // }
-    // if (info == "info2") {
-    //   // this.contenidoInfo = queHacemos;
-    // }
-    // if (info == "info3") {
-    //   // this.contenidoInfo = porqueLoHacemos;
-    // }
-    // if (info == "info4") {
-    //   // this.contenidoInfo = nuestrosObjetivos;
-    // }
-    // if (info == "info5") {
-    //   // this.contenidoInfo = nuestrosValores;
-    // }
-    // if (info == "info6") {
-    //   // this.contenidoInfo = artistas;
-    //   // this.artistasList = artistasList;
-    // }
+  mostrarInfo(imagen: string, info: string) {
+    this.selectedInfo = info;
     this.imagenActual = imagen;
     console.log("imagenActual: ", this.imagenActual);
   }
