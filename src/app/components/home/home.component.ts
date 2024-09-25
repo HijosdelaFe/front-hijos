6620d1730d45989f0222f292747e11cfdfcc601e
HijosdelaFe <hijosdelafe.web@gmail.com>
2024-09-25 17:27:32 -0300

    agregado link venta entradas concierto 2024

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  compraEntradas(){}
+  compraEntradas() {
+    window.location.href = 'https://ventas.autoentrada.com/events/hijos-de-la-fe';
+  }
 }
